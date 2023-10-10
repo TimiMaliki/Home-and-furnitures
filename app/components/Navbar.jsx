d570c20c7413aeae49c3f259a1d5e08388a840ac
TimiMaliki <malikitimi@gmail.com>
2023-10-10 05:12:07 +0100

    uppermenu

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,12 +4,12 @@ import {FaFacebook,FaTwitter,FaInstagram,FaYoutube,FaTiktok,FaLinkedin,FaLinkedi
 const Navbar = () => {
   return (
    <nav className='w-full h-40 bg-black'>
-   <div className='socials bg-gray-800 w-full h-10 flex justify-between'>
+   <div className='socials bg-gray-800 w-full h-10 flex  justify-between'>
         <div className='logo-Text lg:pl-8 pt-2 text-white visible sm:invisible md:visible lg:visible'>
           <h2>THINK HOME, THINK VENTO</h2>
         </div>
 
-        <div className='flex   pt-2 gap-4 text-white text-lg cursor-pointer'>
+        <div className='flex  pt-2 gap-4 text-white text-lg cursor-pointer'>
           <FaFacebook  className=''/>
           <FaTwitter/>
           <FaInstagram/>
@@ -17,7 +17,7 @@ const Navbar = () => {
           <FaLinkedinIn/>
           <FaTiktok/>
 
-        <div className='flex gap-2  text-white   visible sm:invisible md:visible lg:visible'>
+        <div className='flex lg:gap-2  text-white   visible sm:invisible md:visible lg:visible'>
         <div className='border  -mt-2 bg-white '></div>
           <h2 className=''>CONTACT US</h2>
           <div className='border -mt-2 bg-white'></div>
