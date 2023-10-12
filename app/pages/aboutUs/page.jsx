4c90723f718f27d7d40a2144a308e497b3a7d159
TimiMaliki <malikitimi@gmail.com>
2023-10-12 16:10:58 +0100

    blog-pics

--- app/pages/aboutUs/page.jsx
+++ app/pages/aboutUs/page.jsx
@@ -1,97 +1,120 @@
-import React from 'react'
-import Coin from "../../../public/coin.jpg";
+import React from "react";
+import banner from "../../../public/aboutUs-Img/banner.jpg";
+import VentoImg from "../../../public/aboutUs-Img/about-us-vento.jpg"
 import Image from "next/image";
-import Subscription from '@/app/components/Subscription';
+import Subscription from "@/app/components/Subscription";
+import WaveSvg from "@/app/components/WaveSvg";
+import { FaFacebook,FaInstagramSquare , FaYoutubeSquare, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
 
 const blog = () => {
   return (
     <div>
-      <div className=''>
-      <div className="grid place-content-center">
-      <Image src={Coin} width={"1000px"} height={300} />
+      <div className="w-full h-64 relative">
+      <WaveSvg />
+
+        <Image src={banner} className=" w-full h-full object-cover " />
+        <div className="absolute top-0 left-24 mt-16">
+          <h1 className="text-center text-white text-5xl font-extrabold">We are a Luxury Furniture Manufacturing Brand with the vision 
+            of offering innovative and superior quality product to our customers</h1>
+        </div>
       </div>
-     
-    <svg className='-mt-24' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-<path class="elementor-shape-fill" opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"></path>
-<path class="elementor-shape-fill" opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"></path>
-<path class="elementor-shape-fill" d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"></path>
-</svg>
-</div>
 
-<div className='card  p-24 grid place-content-center mb-16'>
-  <div className='w-96 h-80 b rounded-lg border  relative'>
-  <Image src={Coin}  className='absolute w-96 h-80  rounded-lg border-blue-900' />
-  </div>
-</div>
+      <div className="card grid place-content-center mb-16">
+        <div className="w-96 h-80 rounded-lg mt-28 relative">
+          <Image src={VentoImg} className="absolute w-[600px] h-[300px]  rounded-lg " />
+        </div>
+      </div>
 
+      <div className="p-28">
+        <p className="mb-4">
+          Vento Furniture was established in 2002 in Turkey and decided to carry
+          its facilities to Nigeria and Canada.
+        </p>
 
-<div className='p-28'>
+        <p className="mb-4">
+          Vento targeted the best quality products to its customers. Vento
+          Furniture always presented the new modern designs developed by its
+          customers taste and always brought the best.
+        </p>
 
-<p className='mb-4'>
-Vento Furniture was established in 2002 in Turkey and decided to carry its facilities to Nigeria and Canada.</p>
+        <p className="mb-4">
+          Our attention to detail ensures that when you purchase a piece of
+          furniture from Vento Furniture, it will provide superior value and
+          comfort. Our products are designed with safety in mind as well,
+          creating pieces that are long-lasting and safe for all family members.
+          We strive to ensure our customers enjoy their experience with us by
+          providing the highest quality product and unbeatable customer service
+          whenever you buy a Vento Furniture Product.
+        </p>
 
-<p className='mb-4'>
-Vento targeted the best quality products to its customers. Vento Furniture always presented the new modern designs developed by its customers taste and always brought the best.
-</p>
+        <p className="mb-4">
+          Vento Furniture got many customers within a short time because we are
+          aware that our customers understand high quality of products very
+          well.
+        </p>
 
-<p className='mb-4'>
-Our attention to detail ensures that when you purchase a piece of furniture from Vento Furniture, it will provide superior value and comfort. Our products are designed with safety in mind as well, creating pieces that are long-lasting and safe for all family members. We strive to ensure our customers enjoy their experience with us by providing the highest quality product and unbeatable customer service whenever you buy a Vento Furniture Product.
-</p>
+        <p className="mb-4">
+          Our main goal is not to sell furniture, but to bring you together with
+          comfortable, stylish and peaceful furniture.
+        </p>
 
-<p className='mb-4'> 
-Vento Furniture got many customers within a short time because we are aware that our customers understand high quality of products very well.
-</p>
+        <p className="mb-4">
+          With the technical support we provide after selling the furniture, we
+          make our customers feel that we are always with them and provide
+          support.
+        </p>
 
-<p className='mb-4'>
-Our main goal is not to sell furniture, but to bring you together with comfortable, stylish and peaceful furniture.
-</p>
+        <p className="mb-4">
+          The leader of the furniture industry, Vento has been a pioneer in the
+          industry and a brand that sets the trends since the day it was
+          founded. It continues on its way without slowing down with the
+          superiority of its production capacity, the importance it attaches to
+          technology in design, and the understanding of merchandising that
+          always prioritizes customer satisfaction.
+        </p>
 
-<p className='mb-4'>With the technical support we provide after selling the furniture, we make our customers feel that we are always with them and provide support.</p>
+        <p className="mb-4">
+          Acting with the awareness of the responsibility of being a leading
+          company in every field in the sector, Vento produces collections at
+          the most affordable and accessible prices for customer needs while
+          realizing designs beyond the age with technology and innovation. Vento
+          Furniture offers you hundreds of products from sitting groups to
+          dining rooms, from bedrooms to children's and teenage rooms, from
+          beds, bases to garden furniture.
+        </p>
 
-<p className='mb-4'>
-The leader of the furniture industry, Vento has been a pioneer in the industry and a brand that sets the trends since the day it was founded. It continues on its way without slowing down with the superiority of its production capacity, the importance it attaches to technology in design, and the understanding of merchandising that always prioritizes customer satisfaction.
-</p>
+        <p className="mb-4">
+          Among the goals of the brand is to keep the brand always young and
+          dynamic, as well as the turnover achieved through sustainable
+          innovative projects. Among the goals of the brand is to keep the brand
+          always young and dynamic, as well as the turnover achieved through
+          sustainable innovative projects.
+        </p>
 
-<p className='mb-4'>
-Acting with the awareness of the responsibility of being a leading company in every field in the sector, Vento produces collections at the most affordable and accessible prices for customer needs while realizing designs beyond the age with technology and innovation. Vento Furniture offers you hundreds of products from sitting groups to dining rooms, from bedrooms to children's and teenage rooms, from beds, bases to garden furniture.
-</p>
+        <p>
+          Vento Furniture also continues its efforts to increase its market
+          share with the number of specialized stores implemented and to reach
+          unreachable consumer profiles.
+        </p>
+      </div>
 
-<p className='mb-4'>
-Among the goals of the brand is to keep the brand always young and dynamic, as well as the turnover achieved through sustainable innovative projects.
-Among the goals of the brand is to keep the brand always young and dynamic, as well as the turnover achieved through sustainable innovative projects.
-</p>
+      <div className="">
+        <div className="border border-gray-200  w-4/5 ml-20  mb-2"></div>
+      </div>
 
-<p>
-Vento Furniture also continues its efforts to increase its market share with the number of specialized stores implemented and to reach unreachable consumer profiles.
-</p>
+      <div className="flex justify-center gap-4 text-xl p-20">
+        <Link href={""}><FaFacebook size={50}/></Link>
+        <Link href={""}><FaTwitter size={50}/></Link>
+        <Link href={""}><FaInstagramSquare size={50}/></Link>
+        <Link href={""}><FaYoutubeSquare size={50}/></Link>
+      </div>
 
-</div>
-
-<div className=''>
-  <div className='border border-gray-200  w-4/5 ml-20  mb-2'></div>
-  </div>
-
-
-<div className='flex justify-center gap-4 text-xl p-20'>
-  <h2>Socials</h2>
-  <h2>Socials</h2>
-  <h2>Socials</h2>
-  <h2>Socials</h2>
-</div>
-
-
-<div className='card  p-24 grid place-content-center mb-16'>
-  <div className='rounded-lg  border border-blue-400'>
-  <Image src={Coin} width={600} height={300} className='rounded-lg' />
-  </div>
-</div>
-
-<div className='p-20'>
-  <Subscription/>
-</div>
-
+      <div className="p-20">
+        <Subscription />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default blog
+export default blog;
--- app/pages/blog/page.jsx
+++ app/pages/blog/page.jsx
@@ -1,28 +1,41 @@
 import React from "react";
-import Coin from "../../../public/coin.jpg";
+import { BiSolidShareAlt } from "react-icons/bi";
 import Image from "next/image";
+import banner from "../../../public/aboutUs-Img/banner.jpg";
+import png from "../../../public/blog-Img/d23ac428f38cb91ba3574ea6a1ce555b.png";
+import manWoman from "../../../public/blog-Img/full-shot-young-man-woman-home-3-768x402.webp";
+import funiture from "../../../public/blog-Img/Furniture-Combines-768x402.webp";
+import Dinner from "../../../public/blog-Img/Dining-Room-Decorate-768x402.webp";
+import Type from "../../../public/blog-Img/Furniture-Types-768x402.webp";
+import Niger from "../../../public/blog-Img/nige-768x540.webp";
+import TypeColor from "../../../public/blog-Img/The-Rise-of-Modern-768x402.webp";
+import Colors from "../../../public/blog-Img/Preferred-Furniture-Colors-and-Styles-in-Nigeria-768x402.webp"
+import Tradition from "../../../public/blog-Img/Traditional-Furniture-Designs-of-Nigeria-768x402.webp";
+import StyleWoman from "../../../public/blog-Img/stylish-young-woman-using-digital-tablet-restaurant-table-1-768x402.webp"
 
 const page = () => {
   return (
     <div className="w-full h-full">
-      <div className="w-full h-full">
+        
         <div className="relative">
-          {" "}
-          <Image src={Coin} className="w-full h-48 object-cover" />
-          <h1 className="absolute -mt-28 text-gray-300 text-4xl left-64">
-            BLOG
+          <Image src={banner} className="w-full h-48 object-cover" />
+          <div className="absolute top-0 mt-16 grid left-1/2">
+          <h1 className="text-gray-50 font-extrabold text-5xl left-64">
+            Blog
           </h1>
+          </div>
+         
         </div>
-      </div>
+      
 
       {/* card-one */}
       <div className="p-20 mt-20 w-full h-full  columns-3 gap-8">
         <div className="card w-full  rounded">
           {/* card-image-body */}
           <div className="card-image">
-            <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+            <div className="image-body ">
+              <Image src={StyleWoman} className="w-[700px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute left-32 -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -36,15 +49,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -60,9 +69,9 @@ const page = () => {
         <div className="card w-full rounded">
           {/* card-image-body */}
           <div className="card-image">
-            <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+            <div className="image-body ">
+              <Image src={manWoman} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute  lg:ml-20 -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -76,15 +85,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -100,9 +105,9 @@ const page = () => {
         <div className="card w-full  rounded">
           {/* card-image-body */}
           <div className="card-image">
-            <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+            <div className="image-body ">
+              <Image src={funiture} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute  lg:ml-20  -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -116,15 +121,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -144,8 +145,8 @@ const page = () => {
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+              <Image src={Dinner} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-20  -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -159,15 +160,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -177,6 +174,7 @@ const page = () => {
             <h1 className="text-xl font-bold text-red-800">
               CONTINUE READING...
             </h1>
+           
           </div>
         </div>
 
@@ -184,8 +182,8 @@ const page = () => {
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+              <Image src={Type} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-20 -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -199,15 +197,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -224,8 +218,8 @@ const page = () => {
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+              <Image src={TypeColor} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-20  -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -239,15 +233,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -263,14 +253,13 @@ const page = () => {
 
      {/* card-three */}
 
-
      <div className="p-20 mt-20 w-full h-full  columns-3 gap-8">
         <div className="card w-full  rounded">
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+              <Image src={Tradition } className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-20  -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -284,15 +273,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+            <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -309,8 +294,8 @@ const page = () => {
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+              <Image src={Colors} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute  lg:ml-20  -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -324,15 +309,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -349,8 +330,8 @@ const page = () => {
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
-              <Image src={Coin} className="w-[500px] h-48 object-contain" />
-              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+              <Image src={Niger} className="w-[500px] h-48 object-cover" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-20  -mt-2 text-white text-sm bg-red-800 text-center">
                 <h2>Furnitures,Design trends</h2>
               </div>
             </div>
@@ -364,15 +345,11 @@ const page = () => {
           </div>
           {/* card-body */}
           <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
-            <h2 className="font-extrabold text-2xl mb-8">
+          <h2 className="font-extrabold text-2xl mt-2">
               Sitting in Style: Exotic Chairs for Every Room
             </h2>
-            <div className="flex justify-center gap-8 mb-8">
-              <p>By</p>
-              <p>Icons</p>
-              <p>Vendatta</p>
-              <p>Icon</p>
-              <p>Icon</p>
+            <div className="flex justify-center gap-8 mb-4">
+             <BiSolidShareAlt size={30} className="text-gray-300"/>
             </div>
             <p className="mb-4">
               As long as people have been furnishing homes, they've been
@@ -385,6 +362,7 @@ const page = () => {
           </div>
         </div>
       </div>
+    
 
     </div>
   );
