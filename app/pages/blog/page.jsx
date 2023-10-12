a348e7d4c9332ed685e62532ed26259f4c6c7bd1
TimiMaliki <malikitimi@gmail.com>
2023-10-12 12:32:59 +0100

    blog

--- app/pages/blog/page.jsx
+++ app/pages/blog/page.jsx
@@ -15,8 +15,9 @@ const page = () => {
         </div>
       </div>
 
-      <div className="p-20 mt-20 w-full h-full border border-red-400 grid">
-        <div className="card w-2/5 h-full rounded">
+      {/* card-one */}
+      <div className="p-20 mt-20 w-full h-full  columns-3 gap-8">
+        <div className="card w-full  rounded">
           {/* card-image-body */}
           <div className="card-image">
             <div className="image-body bg-black">
@@ -50,20 +51,341 @@ const page = () => {
               designing (and shopping for) chairs to go in them. And...
             </p>
 
-            <h1 className="text-xl font-bold text-red-800">CONTINUE READING...</h1>
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
           </div>
-
-
         </div>
 
+        <div className="card w-full rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
 
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
 
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
 
+        <div className="card w-full  rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
 
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
 
-
-
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
       </div>
+
+      {/* card-two  */}
+      <div className="p-20 mt-20 w-full h-full  columns-3  gap-8">
+        <div className="card w-full  rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
+
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
+
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
+
+        <div className="card w-full rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
+
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
+
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
+
+        <div className="card w-full  rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
+
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
+
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
+      </div>
+
+     {/* card-three */}
+
+
+     <div className="p-20 mt-20 w-full h-full  columns-3 gap-8">
+        <div className="card w-full  rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
+
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
+
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
+
+        <div className="card w-full rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
+
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
+
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
+
+        <div className="card w-full  rounded">
+          {/* card-image-body */}
+          <div className="card-image">
+            <div className="image-body bg-black">
+              <Image src={Coin} className="w-[500px] h-48 object-contain" />
+              <div className="lg:w-64 h-8 rounded absolute lg:ml-28 -mt-2 text-white text-sm bg-red-800 text-center">
+                <h2>Furnitures,Design trends</h2>
+              </div>
+            </div>
+
+            <div className="w-16 h-16 ml-4  -mt-44 absolute  bg-gray-300 rounded">
+              <div className="grid place-content-center">
+                <h2>18</h2>
+                <h1>Sept</h1>
+              </div>
+            </div>
+          </div>
+          {/* card-body */}
+          <div className="card-body grid place-content-center p-20 h-80 border border-gray-300 ">
+            <h2 className="font-extrabold text-2xl mb-8">
+              Sitting in Style: Exotic Chairs for Every Room
+            </h2>
+            <div className="flex justify-center gap-8 mb-8">
+              <p>By</p>
+              <p>Icons</p>
+              <p>Vendatta</p>
+              <p>Icon</p>
+              <p>Icon</p>
+            </div>
+            <p className="mb-4">
+              As long as people have been furnishing homes, they've been
+              designing (and shopping for) chairs to go in them. And...
+            </p>
+
+            <h1 className="text-xl font-bold text-red-800">
+              CONTINUE READING...
+            </h1>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
