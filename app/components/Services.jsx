2c0505ae4f0fe0087ee705d40ff20d577b848e2b
TimiMaliki <malikitimi@gmail.com>
2023-10-13 15:51:59 +0100

    adding Imagies

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+
 const Services = () => {
   return (
-    <div className='lg:flex items-center gap-8 justify-between px-20'>
+    <div className='lg:flex items-center gap-8 justify-between lg:px-20'>
 
         <div className='grid place-content-center'>
-            <h1 className='text-3xl mb-4 text-center'>Logo</h1>
+        
             <h1 className='text-3xl mb-4 text-center'>Fast Delivery</h1>
             <p className=' text-md lg:text-lg w-40 lg:w-80 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsum nam dolores!</p>
         </div>
