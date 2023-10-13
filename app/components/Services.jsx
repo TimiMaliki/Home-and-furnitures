0939344eb2694d85608eeec60d30a4073ba8f317
TimiMaliki <malikitimi@gmail.com>
2023-10-13 16:10:50 +0100

    adding Imagies

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,32 +1,68 @@
-import React from 'react'
+"use client"
 
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import Image from "next/image";
+import FastDelivery from "../../public/service-Img/express-delivery.png";
+import BestQuality from "../../public/service-Img/medal.png";
+import Install from "../../public/service-Img/process.png";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Services = () => {
   return (
-    <div className='lg:flex items-center gap-8 justify-between lg:px-20'>
+    <div className="lg:flex items-center gap-2  justify-between lg:px-20">
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid place-content-center ">
+            <Image src={FastDelivery} width={50} className="mb-4" />
+            <h1 className="lg:text-3xl mb-4 ">Fast Delivery</h1>
+            <p className=" text-md lg:text-lg w-40 lg:w-80 h-32">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              ipsum nam dolores!
+            </p>
+          </div>
+        </SwiperSlide>
 
-        <div className='grid place-content-center'>
-        
-            <h1 className='text-3xl mb-4 text-center'>Fast Delivery</h1>
-            <p className=' text-md lg:text-lg w-40 lg:w-80 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsum nam dolores!</p>
-        </div>
+        <SwiperSlide>
+          <div className="grid place-content-center">
+            <Image src={BestQuality} width={50} className="mb-4" />
+            <h1 className="text-3xl mb-4">Fast Delivery</h1>
+            <p className=" text-md lg:text-lg w-40 lg:w-80 h-32">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              ipsum nam dolores!
+            </p>
+          </div>
+        </SwiperSlide>
 
-        <div className='grid place-content-center'>
-            <h1 className='text-3xl mb-4 text-center'>Logo</h1>
-            <h1 className='text-3xl mb-4 text-center'>Fast Delivery</h1>
-            <p className=' text-md lg:text-lg w-40 lg:w-80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsum nam dolores!</p>
-        </div>
-
-        <div className='grid place-content-center'>
-            <h1 className='text-3xl mb-4 text-center'>Logo</h1>
-            <h1 className='text-3xl mb-4 text-center'>Fast Delivery</h1>
-            <p className=' text-md lg:text-lg w-40 lg:w-80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsum nam dolores!</p>
-        </div>
-
+        <SwiperSlide>
+          <div className="grid place-content-center">
+            <Image src={Install} width={50} className="mb-4" />
+            <h1 className="text-3xl mb-4">Fast Delivery</h1>
+            <p className=" text-md lg:text-lg w-40 lg:w-80 h-32">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              ipsum nam dolores!
+            </p>
+          </div>
+        </SwiperSlide>
 
 
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
