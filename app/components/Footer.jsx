6d6a4076d5a33474a17525db24c2fa850ef67cf5
TimiMaliki <malikitimi@gmail.com>
2023-10-18 14:26:45 +0100

    fixed the responsiveness

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,33 +1,44 @@
 import React from "react";
-import facebook from "../../public/aboutUs-Img/facebook-1-768x768.png"
-import Instagram from "../../public/aboutUs-Img/instagram-1-768x768.png"
-import twitter from "../../public/aboutUs-Img/twitter-1-768x768.png"
-import youtube from "../../public/aboutUs-Img/youtube-1-768x768.png"
-import   ventoBanner from "../../public/home-Img/logo-black.png"
+import facebook from "../../public/aboutUs-Img/facebook-1-768x768.png";
+import Instagram from "../../public/aboutUs-Img/instagram-1-768x768.png";
+import twitter from "../../public/aboutUs-Img/twitter-1-768x768.png";
+import youtube from "../../public/aboutUs-Img/youtube-1-768x768.png";
+import ventoBanner from "../../public/home-Img/logo-black.png";
 import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className=" w-full h-full bg-gray-100 gap-8 grid grid-cols-1 px-20 lg:px-0  sm:grid-cols-2 md:grid-cols-5  lg:grid-cols-5">
+    <div className=" w-full h-full bg-gray-100 gap-8 grid grid-cols-1 px-0 lg:px-20 lg:px-0  sm:grid-cols-2 md:grid-cols-5  lg:grid-cols-5">
       <div className="w-full h-full p-20">
-      <Image src={ventoBanner} width={100}  className="mb-8" alt="..."/>
+        <Image src={ventoBanner} width={100} className="mb-8" alt="..." />
 
         <div className="flex gap-4 justify-center mb-4">
-         <Image  src={facebook} width={50} alt="..."/>
-         <Image  src={Instagram} width={50} alt="..."/>
-         <Image  src={twitter} width={50} alt="..."/>
-         <Image  src={youtube} width={50} alt="..."/>
+          <Image src={facebook} width={50} alt="..." />
+          <Image src={Instagram} width={50} alt="..." />
+          <Image src={twitter} width={50} alt="..." />
+          <Image src={youtube} width={50} alt="..." />
         </div>
 
-        <h1 className="text-lg text-center mb-8">FURNITURE CATEGORY</h1>
-        <ul className="text-center ">
-          <li>Bedroom Sets</li>
-          <li>Sofa Sets</li>
-          <li>Dining Sets</li>
-          <li>Office Furniture</li>
-          <li>TV Stand</li>
-          <li>Accessories</li>
-        </ul>
+        <h1 className="text-sm lg:text-lg text-center mb-8">
+          FURNITURE CATEGORY
+        </h1>
+        <div className="grid grid-cols-2 gap-10">
+          <div>
+           <ul className="text-sm lg:text-xl">
+           <li>Bedroom Sets</li>
+            <li>Sofa Sets</li>
+            <li>Dining Sets</li>
+           </ul>
+          </div>
+
+          <div>
+           <ul className="text-sm lg:text-xl">
+           <li>Office Furniture</li>
+            <li>TV Stand</li>
+            <li>Accessories</li>
+           </ul>
+          </div>
+        </div>
       </div>
 
       <div className="w-full h-full ">
@@ -71,7 +82,9 @@ const Footer = () => {
 
       <div className="w-full h-full">
         <div className="border border-gray-200  w-full mt-20 mb-8"></div>
-        <h1 className="mb-8 font-extrabold cursor-pointer text-2xl">PORT HARCOURT</h1>
+        <h1 className="mb-8 font-extrabold cursor-pointer text-2xl">
+          PORT HARCOURT
+        </h1>
         <div className="border border-gray-200 mb-10"></div>
 
         <div className="grid">
@@ -87,8 +100,6 @@ const Footer = () => {
         </div>
       </div>
 
-
-
       <div className="w-full h-full">
         <div className="border border-gray-200  w-full mt-20 mb-8"></div>
         <h1 className="mb-8 font-extrabold cursor-pointer text-2xl">CANADA</h1>
@@ -96,7 +107,7 @@ const Footer = () => {
 
         <div className="grid">
           <p className="mb-8 hover:font-extrabold cursor-pointer">
-          4326 Walker Rd, Windsor, ON N8W 3T5 Canada
+            4326 Walker Rd, Windsor, ON N8W 3T5 Canada
           </p>
           <p className="mb-4  cursor-pointer">WORKING HOURS</p>
           <p className="mb-6  cursor-pointer">
@@ -106,8 +117,6 @@ const Footer = () => {
           <p className=" cursor-pointer">+1 (519) 250-0606</p>
         </div>
       </div>
-
-
     </div>
   );
 };
