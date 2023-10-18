c87b3ba6a88b9af563ab5b04e29c94d6230a8834
TimiMaliki <malikitimi@gmail.com>
2023-10-18 13:25:32 +0100

    fixed the responsiveness

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, HashNavigation } from "swiper/modules";
+import { Autoplay,Pagination, Navigation} from "swiper/modules";
 import Image from "next/image";
 import woodenImg from "../../public/home-Img/WOODEN-BEDROOM-SET.webp";
 import NaturalImg from "../../public/home-Img/NATURA-DINING-SET-1kapak_1_11zon.jpg.webp";
@@ -16,23 +16,25 @@ const Hero = () => {
   return (
     <div className="mt-4">
       <Swiper
-        spaceBetween={30}
-        hashNavigation={{
-          watchState: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation, HashNavigation]}
-        className="mySwiper"
+         spaceBetween={30}
+         centeredSlides={true}
+         autoplay={{
+           delay: 2500,
+           disableOnInteraction: false,
+         }}
+         pagination={{
+           clickable: true,
+         }}
+         navigation={true}
+         modules={[Autoplay, Pagination, Navigation]}
+         className="mySwiper"
       >
         <SwiperSlide>
           <div className="grid items-center text-center w-full h-full relative">
-            <Image src={woodenImg} style={{ width: "100%", height: "900px" }} alt="..." />
-            <div className="-translate-y-[40rem] lg:mr-[30rem] lg:text-6xl font-extrabold text-white">
-              <h1 className="mb-8">WOODEN BEDROOM SET</h1>
-              <p className="mb-8  lg:text-xl">
+            <Image src={woodenImg} style={{ width: "100%", height: "900px"  , objectFit:'cover'}} alt="..." />
+            <div className="-translate-y-[40rem] lg:mr-[30rem]font-extrabold text-white">
+              <h1 className="mb-8  text-6xl ">WOODEN BEDROOM SET</h1>
+              <p className="mb-8  text-xl">
              Every Detail has been carefully considered for your comfort
               </p>
               </div>
@@ -43,12 +45,12 @@ const Hero = () => {
           <div className="grid items-center text-center w-full h-96">
             <Image
               src={NaturalImg}
-              style={{ width: "100%", height: "900px" }}
+              style={{ width: "100%", height: "900px" , objectFit:'cover'}}
               alt="..."
             />
-            <div className="-translate-y-[40rem] lg:mr-[45rem] lg:text-4xl font-extrabold text-white ">
+            <div className="-translate-y-[40rem] lg:mr-[45rem] text-4xl font-extrabold text-white ">
               <h1 className="mb-8">Furniture Collection 2023</h1>
-              <p className="mb-8  lg:text-6xl">
+              <p className="mb-8  text-6xl">
                 Natural Dinning Set
               </p>
               </div>
@@ -57,10 +59,10 @@ const Hero = () => {
 
         <SwiperSlide>
           <div className="grid items-center text-center w-full h-96">
-            <Image src={picazoImg} style={{ width: "100%", height: "900px" }} alt="..."/>
-            <div className="-translate-y-[40rem] lg:mr-[30rem]  font-extrabold text-white ">
-              <h1 className="mb-8 lg:text-xl">NEW ARRIVALS COLLECTION  2023</h1>
-              <p className="mb-8  lg:text-6xl">
+            <Image src={picazoImg} style={{ width: "100%", height: "900px"  , objectFit:'cover'}} alt="..."/>
+            <div className="-translate-y-[40rem] lg:mr-[26rem]  font-extrabold text-white ">
+              <h1 className="mb-8 text-xl">NEW ARRIVALS COLLECTION  2023</h1>
+              <p className="mb-8  text-4xl">
                 MODERN FURNITURES
               </p>
               <p>A Contemporary master pieces</p>
