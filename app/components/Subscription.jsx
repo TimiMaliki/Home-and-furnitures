f6ea8c78a1f92a1d70e701ad6e84f2fcfa7e65f8
TimiMaliki <malikitimi@gmail.com>
2023-10-18 14:07:44 +0100

    fixed the responsiveness

--- app/components/Subscription.jsx
+++ app/components/Subscription.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Subscription = () => {
   return (
     <div className="grid place-content-center p-28">
-      <h1 className="text-3xl font-semibold mb-8 text-center">
+      <h1 className="text-xl lg:text-3xl font-semibold mb-8 text-center">
         Subscribe to our newsletter
       </h1>
 
-      <div className="flex gap-8 mb-4">
-      <input type="text" placeholder='E-mail'  className='border border-gray-300 hover:border-blue-300 text-xl rounded w-full h-10 px-4'/>
-      <button className="border border-gray-400 w-40 hover:bg-gray-300 ">SUBMIT</button>
+      <div className="lg:flex p-8 lg:p-0 lg:gap-8 mb-4">
+      <input type="text" placeholder='E-mail'  className='border border-gray-300 hover:border-blue-300 text-lg lg:text-xl rounded w-full h-10 px-4'/>
+      <button className="rounded border border-gray-900 lg:border-gray-400 w-40 hover:bg-gray-300  mt-4 lg:mt-0">SUBMIT</button>
       </div>
 
-      <p className="">
+      <p className="text-sm lg:text-lg">
       Stay in the loop with the latest updates and exclusive offers by subscribing to our newsletter
       </p>
       
